57f6de23c532e6ed52e8ca35ca24128631b78d01
Nikita Shevelev <nikita.shevelev@ripecode.dev>
2021-10-03 22:05:45 +0300

    Initial commit

---
+++ labs/lab1.py
@@ -0,0 +1,40 @@
+import main
+
+
+def task8():
+    print('This is task8')
+
+
+def task9():
+    print('This is task9')
+
+
+def task10():
+    print('This is task10')
+
+
+def print_menu():
+    print('Наберите выбрав задачу: \n\n'
+          '1 - задача #8\n'
+          '2 - задача #9\n'
+          '3 - задача #10\n'
+          '0 - для выхода в главное меню\n')
+
+
+def start_lab1():
+    main.clear_console()
+    while True:
+        print_menu()
+        menu_choice = main.input_menu_choice()
+
+        if menu_choice == 1:
+            return task8()
+        if menu_choice == 2:
+            return task9()
+        if menu_choice == 3:
+            return task10()
+        if menu_choice == 0:
+            return main.start_main_menu()
+        else:
+            main.print_wrong_input_choice()
+
---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import labs.lab1 as lab1
+
+
+def clear_console():
+    os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+
+
+def print_menu():
+    print('Наберите выбрав лабораторную работу: \n\n'
+          '1 - лабораторная работа #1\n'
+          '0 - для выхода из программы\n')
+
+
+def print_wrong_input_choice():
+    print('\n-- Такого варианта выбора не существует. Попробуйте снова --\n')
+
+
+def input_menu_choice():
+    try:
+        input_choice = int(input())
+    except ValueError:
+        return 'Повторите ввод, то что вы ввели - не число\n\n'
+    else:
+        return int(input_choice)
+
+
+def start_main_menu():
+    while True:
+        print_menu()
+        menu_choice = input_menu_choice()
+
+        if menu_choice == 1:
+            return lab1.start_lab1()
+        if menu_choice == 0:
+            raise SystemExit(1)
+        else:
+            print_wrong_input_choice()
+
+
+if __name__ == '__main__':
+    start_main_menu()
